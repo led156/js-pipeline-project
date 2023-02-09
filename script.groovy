231eb3ea44947b3c3d49eb4240f11d55813028db
eunbi lee <led156@naver.com>
2023-02-09 15:40:27 +0900

    fasjio

--- script.groovy
+++ script.groovy
@@ -1,30 +1,14 @@
-pipeline {
-   agent any
-   parameters {
-      choice(name: 'VERSION', choices: ['1.1.0','1.2.0','1.3.0'], description: '')
-      booleanParam(name: 'executeTests', defaultValue: true, description: '')
-   }
-   stages {
-      stage("build") {
-         steps {
-            echo 'building the applicaiton...'
-         }
-      }
-      stage("test") {
-         when {
-            expression {
-               params.executeTests
-            }
-         }
-         steps {
-            echo 'testing the applicaiton...'
-         }
-      }
-      stage("deploy") {
-         steps {
-            echo 'deploying the applicaiton...'
-            echo "deploying version ${params.VERSION}"
-         }
-      }
-   }
+
+def buildApp() {
+   echo 'building the applications...'
 }
+
+def testApp() {
+   echo 'testing the applications...'
+}
+
+def deployApp() {
+   echo 'deploying the applicaiton...'
+   echo "deploying version ${params.VERSION}"
+}
+return this
